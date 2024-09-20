a43f44c121fee1b57ac67e7b0feb78a1adad0238
Nicolas Rea <nrea@made-f0Br3s6.clusters.42paris.fr>
2024-09-20 17:13:22 +0200

    gestion des erreurs
    changement de == en != pour recuperer le bon type

--- nrea/response_error.cpp
+++ nrea/response_error.cpp
@@ -3,14 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   response_error.cpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qgiraux <qgiraux@student.42.fr>            +#+  +:+       +#+        */
+/*   By: nrea <nrea@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 10:13:31 by nrea              #+#    #+#             */
-/*   Updated: 2024/09/20 16:37:48 by qgiraux          ###   ########.fr       */
+/*   Updated: 2024/09/20 17:13:02 by nrea             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headerParser.hpp"
+
+std::string dummy_get_customerrorPage(void)
+{
+	// la vraie fonction renvoie un path complet ou "" si
+	// il faut generer une erreur
+	return "";
+}
 
 /* returns a header_infos set to the appropriate error*/
 header_infos response_error(std::string error_code, ConfigServer  & config,int locationIndex)
@@ -20,8 +27,9 @@ header_infos response_error(std::string error_code, ConfigServer  & config,int l
 	response.returnCode = std::atoi(error_code.c_str());//error_code;
 	response.contentType = "text/html";
 	// response.ressourcePath =  config.getCustomErrorPage(locationIndex,std::atoi(error_code.c_str())); //config.getCustomErrorPage(locationIndex, error_code);
-	response.ressourcePath = ""; // pour TEST
-	response.bodySize = getFileSize(response.ressourcePath.c_str());
+	response.ressourcePath = dummy_get_customerrorPage(); // pour TEST
+	if (response.ressourcePath.size())
+		response.bodySize = getFileSize(response.ressourcePath.c_str());
 	response.configServer = &config;
 	response.locationIndex = locationIndex;
 
