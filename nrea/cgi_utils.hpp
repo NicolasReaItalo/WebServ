4f1a77e43ce732a7c0181492adad867a6fab599f
Nicolas Rea <nrea@made-f0Br3s6.clusters.42paris.fr>
2024-10-02 10:36:41 +0200

    CGI_UTILS ajout freeEnv

--- nrea/cgi_utils.hpp
+++ nrea/cgi_utils.hpp
@@ -6,7 +6,7 @@
 /*   By: nrea <nrea@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 17:17:29 by nrea              #+#    #+#             */
-/*   Updated: 2024/10/01 14:09:32 by nrea             ###   ########.fr       */
+/*   Updated: 2024/10/02 10:36:28 by nrea             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 
 
 char **MapToEnv(std::map<std::string,std::string> const & map);
+void freeEnv(char **env);
 std::string	detect_cgi(std::string const &uri, std::vector<std::string> const &cgi_ext);
 void stripPathInfo(std::string &uri, std::string &path_info, std::string cgi);
 std::string cgi_convert(std::string header_attribute);
